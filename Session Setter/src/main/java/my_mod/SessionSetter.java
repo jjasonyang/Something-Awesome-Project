0763556d42844c47f96e0c57aa0b6078019e1a24
Jason Yang <jason.yang2680@gmail.com>
2024-11-11 13:51:03 +1100

    changed mod name to SessionSetter

--- Session Setter/src/main/java/my_mod/SessionSetter.java
+++ Session Setter/src/main/java/my_mod/SessionSetter.java
@@ -6,15 +6,15 @@ import net.minecraftforge.fml.common.Mod;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(ExampleMod.MODID)
-public class ExampleMod
+@Mod(SessionSetter.MODID)
+public class SessionSetter
 {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "examplemod";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    public ExampleMod()
+    public SessionSetter()
     {
         Multiplayer_Button multiplayer_button = new Multiplayer_Button();
         MinecraftForge.EVENT_BUS.register(multiplayer_button);
